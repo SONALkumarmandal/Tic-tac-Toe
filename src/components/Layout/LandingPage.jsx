3ca5f48d97b379e3c3df0df07244aac634d912d4
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2025-04-14 14:31:26 +0530

    small changes

--- src/components/Layout/LandingPage.jsx
+++ src/components/Layout/LandingPage.jsx
@@ -4,8 +4,8 @@ import Tasks from '../Righthalf/Tasks'
 function LandingPage() {
   return (
     <div className='bg-blend-darken h-screen w-full flex flex-col md:w-full lg:w-full overflow-y-hidden'>
-        <div className='w-full  md:w-full lg:w-full h-24 flex justify-center items-center text-3xl text-white font-extrabold'>To-Do App</div>
-        <div className='flex flex-col md:flex-row lg:flex-row mt-12 p-8 gap-8 h-screen '>
+        <div className='w-full  md:w-full lg:w-full h-24 flex justify-center items-center text-3xl text-white font-extrabold mt-8'>To-Do App</div>
+        <div className='flex flex-col md:flex-row lg:flex-row mt-18 p-8 gap-8 h-screen '>
         <AddTask />
         <Tasks />
         </div>
--- src/components/Lefthalf/AddTask.jsx
+++ src/components/Lefthalf/AddTask.jsx
@@ -29,7 +29,7 @@ function AddTask() {
         </div>
         <div className='h-5/6 w-full  flex flex-col pt-10 items-center text-center gap-4'>
             <input value={text} onChange={handleChange} className='bg-white w-2/3 h-2/4 border-2 border-black rounded-md font-mono text-black text-center text-wrap' type="text" name="" id="" />
-            <button onClick={handleAdd} className='bg-blue-500 rounded-2xl w-2/3 h-1/4 cursor-pointer hover:bg-blue-600' type="submit">Add</button>
+            <button onClick={handleAdd} className='bg-blue-500 rounded-xl w-2/3 h-2/4 cursor-pointer hover:bg-blue-600' type="submit">Add</button>
         </div>
         </div>
     </div>
